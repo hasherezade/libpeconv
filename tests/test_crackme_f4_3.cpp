fa55591bfe6cc6b62867e59d2d84759e39653331
hasherezade <hasherezade@gmail.com>
2017-11-24 16:58:47 +0100

    [REFACT][tests] Added namespace for test3

--- tests/test_crackme_f4_3.cpp
+++ tests/test_crackme_f4_3.cpp
@@ -6,34 +6,39 @@
 #include "resource_helper.h"
 #include "resource.h"
 
-BYTE *g_Buffer = NULL;
-const size_t g_BufferLen = 0x79;
+#include "peconv.h"
 
-BYTE g_Buffer2[g_BufferLen] = { 0 };
+namespace test3 {
+    BYTE *g_Buffer = NULL;
+    const size_t g_BufferLen = 0x79;
 
-WORD (*calc_checksum) (BYTE *decoded_buffer, size_t buf_size) = NULL;
+    BYTE g_Buffer2[g_BufferLen] = { 0 };
 
-bool test_val(BYTE xor_val)
-{
-    for (size_t i = 0; i < g_BufferLen; i++) {
-        BYTE val = g_Buffer[i];
-        g_Buffer2[i] = (xor_val ^ val) + 0x22;
+    WORD (*calc_checksum) (BYTE *decoded_buffer, size_t buf_size) = NULL;
+
+    bool test_val(BYTE xor_val)
+    {
+        for (size_t i = 0; i < g_BufferLen; i++) {
+            BYTE val = g_Buffer[i];
+            g_Buffer2[i] = (xor_val ^ val) + 0x22;
+        }
+        WORD checksum = calc_checksum(g_Buffer2, g_BufferLen);
+        if (checksum == 0xfb5e) {
+            return true;
+        }
+        return false;
     }
-    WORD checksum = calc_checksum(g_Buffer2, g_BufferLen);
-    if (checksum == 0xfb5e) {
-        return true;
-    }
-    return false;
-}
 
-BYTE brutforce()
-{
-    BYTE xor_val = 0;
-    do {
-      xor_val++;
-    } while (!test_val(xor_val));
-    return xor_val;
-}
+    BYTE brutforce()
+    {
+        BYTE xor_val = 0;
+        do {
+          xor_val++;
+        } while (!test_val(xor_val));
+        return xor_val;
+    }
+};
+
 //---
 
 int tests::deploy_crackme_f4_3()
@@ -59,12 +64,12 @@ int tests::deploy_crackme_f4_3()
         free_resource_data(raw_crackme, raw_size);
     }//!scope1
 
-    g_Buffer = (BYTE*) (0x107C + (ULONGLONG) loaded_pe);
+    test3::g_Buffer = (BYTE*) (0x107C + (ULONGLONG) loaded_pe);
 
     ULONGLONG func_offset = 0x11e6 + (ULONGLONG) loaded_pe;
-    calc_checksum =  ( WORD (*) (BYTE *, size_t ) ) func_offset;
+    test3::calc_checksum =  ( WORD (*) (BYTE *, size_t ) ) func_offset;
 
-    BYTE found = brutforce();
+    BYTE found = test3::brutforce();
     printf("Found: %x\n", found);
     int res = -1;
     if (found == 0xa2) {
--- tests/test_crackme_f4_3.h
+++ tests/test_crackme_f4_3.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "peconv.h"
-
 namespace tests {
     // Loads the FlareOn4 Crackme 3, brutforces the key value using a function imported from the crackme and verifies it
     int deploy_crackme_f4_3();
