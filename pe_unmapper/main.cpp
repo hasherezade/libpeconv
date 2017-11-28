04373c9aeb4eba5c98f8ede2d07b2565be79e93a
hasherezade <hasherezade@gmail.com>
2017-11-28 19:57:26 +0100

    [FEATURE][pe_unmapper] Added remapping from raw to virtual

--- pe_unmapper/main.cpp
+++ pe_unmapper/main.cpp
@@ -3,7 +3,7 @@
 #include "peconv.h"
 using namespace peconv;
 
-bool remap_pe_file(IN const char* filename, IN const char* out_filename, ULONGLONG loadBase)
+bool remap_pe_file(IN const char* filename, IN const char* out_filename, ULONGLONG loadBase, bool mode_v_to_r)
 {
     if (filename == NULL || out_filename == NULL) return false;
     //Read input module:
@@ -24,8 +24,22 @@ bool remap_pe_file(IN const char* filename, IN const char* out_filename, ULONGLO
     fclose(f);
     f = NULL;
 
-    size_t raw_size = 0;
-    BYTE* out_buf = pe_virtual_to_raw(in_buf, size, loadBase, raw_size, false);
+    BYTE* out_buf = NULL;
+    
+    size_t out_size = 0;
+    if (mode_v_to_r) {
+        printf("Virtual to raw:\n");
+        out_buf = peconv::pe_virtual_to_raw(in_buf, size, loadBase, out_size, false);
+    } else {
+        printf("Raw to virtual:\n");
+        out_buf = peconv::load_pe_module(in_buf, size, out_size, false, false);
+        if (out_buf) {
+           ULONGLONG base = peconv::get_image_base(out_buf);
+           if (!relocate_module(out_buf, out_size, (ULONGLONG) base)) {
+               printf("Could not relocate the module!\n");
+           }
+        }
+    }
     if (!out_buf) {
          VirtualFree(in_buf, size, MEM_RELEASE);
          return false;
@@ -34,13 +48,13 @@ bool remap_pe_file(IN const char* filename, IN const char* out_filename, ULONGLO
     bool isOk = false;
     f = fopen(out_filename, "wb");
     if (f) {
-        fwrite(out_buf, 1, raw_size, f);
+        fwrite(out_buf, 1, out_size, f);
         fclose(f);
         isOk = true;
     }
 
     VirtualFree(in_buf, size, MEM_RELEASE);
-    free_pe_buffer(out_buf, raw_size);
+    free_pe_buffer(out_buf, out_size);
 
     return isOk;
 }
@@ -48,14 +62,20 @@ bool remap_pe_file(IN const char* filename, IN const char* out_filename, ULONGLO
 
 int main(int argc, char *argv[])
 {
-    char* version = "0.2";
+    char* version = "0.3";
     char*  filename = NULL;
     char* out_filename = "out.exe";
+    const char mode_switch[] = "-v";
     ULONGLONG loadBase = 0;
+    bool mode_v_to_r = true;
     if (argc < 3) {
         printf("[ pe_unmapper v%s ]\n\n", version);
-        printf("Args: <input file> <load base: in hex> [*output file]\n");
+        printf("Args: <input file> <load base: in hex> [*output file] [*%s]\n", mode_switch);
         printf("* - optional\n");
+        printf("---\n");
+        printf("Default mode:\n\tvirtual input -> raw output\n", mode_switch);
+        printf("%s : switches remapping mode into:\n\traw input -> virtual output\n", mode_switch);
+        printf("---\n");
         system("pause");
         return -1;
     }
@@ -65,10 +85,18 @@ int main(int argc, char *argv[])
     }
 
     if (argc > 3) {
-        out_filename = argv[3];
+        if (strcmp(argv[3], mode_switch) != 0) {
+            out_filename = argv[3];
+        }
     }
-    //BYTE* pe_virtual_to_raw(BYTE* payload, size_t in_size, ULONGLONG loadBase, size_t &outputSize, bool rebuffer=true);
-    if (remap_pe_file(filename, out_filename, loadBase)) {
+    if (argc > 3) {
+        for (int i = 3; i < argc; i++) {
+            if (strcmp(argv[i], mode_switch) == 0)
+                mode_v_to_r = false;
+        }
+    }
+
+    if (remap_pe_file(filename, out_filename, loadBase, mode_v_to_r)) {
         printf("Success!\n");
         printf("Saved output to: %s\n", out_filename);
     } else {
