e60415aa8cd57fad0901cdf187fc4ef74ac1c964
hasherezade <hasherezade@gmail.com>
2017-12-25 00:44:21 +0100

    [REFACT] Restructured process_explorer (using classes)

--- libpeconv/include/peconv/process_explorer.h
+++ libpeconv/include/peconv/process_explorer.h
@@ -1,14 +1,58 @@
 #pragma once
 
 #include "Windows.h"
-#include <TlHelp32.h>
 
 #include <map>
 
 namespace peconv {
 
-    size_t enum_modules_in_process(std::map<ULONGLONG, MODULEENTRY32> &modulesMap, DWORD process_id=0);
+    struct LoadedModule {
 
-    HMODULE get_module_containing(ULONGLONG address, DWORD process_id=0);
+        LoadedModule(ULONGLONG _start, ULONGLONG _end, std::string _path, DWORD _pid)
+            :start(_start), end(_end), path(_path), process_id(_pid)
+        {
+        }
+
+        ~LoadedModule()
+        {
+        }
+
+        bool operator<(LoadedModule other) const
+        {
+            return this->start < other.start;
+        }
+
+        bool is_remote()
+        {
+            return process_id != GetCurrentProcessId();
+        }
+
+        std::string path;
+        ULONGLONG start;
+        ULONGLONG end;
+        DWORD process_id;
+    };
+
+    struct ProcessModules {
+        ProcessModules (DWORD _pid)
+            : process_id(_pid)
+        {
+        }
+        
+        ~ProcessModules()
+        {
+            delete_mapping();
+        }
+
+        void delete_mapping();
+        size_t load_mapping();
+        const LoadedModule* get_module_containing(ULONGLONG address);
+
+        std::map<ULONGLONG, LoadedModule*> modulesMap;
+        DWORD process_id;
+    };
+
+    //wrapper - works for a current process:
+    HMODULE get_module_containing(ULONGLONG address);
 
 }; //namespace peconv
--- libpeconv/src/process_explorer.cpp
+++ libpeconv/src/process_explorer.cpp
@@ -1,8 +1,11 @@
 #include "peconv/process_explorer.h"
 
+#include <string>
 #include <iostream>
 
-size_t peconv::enum_modules_in_process(std::map<ULONGLONG, MODULEENTRY32> &modulesMap, DWORD process_id)
+#include <TlHelp32.h>
+
+size_t peconv::ProcessModules::load_mapping()
 {
     if (process_id == 0) {
         process_id = GetCurrentProcessId();
@@ -15,35 +18,61 @@ size_t peconv::enum_modules_in_process(std::map<ULONGLONG, MODULEENTRY32> &modul
         std::cerr << "[ERROR] Fetching modules failed!" << std::endl;
         return 0;
     }
-    size_t modules = 1;
-    modulesMap[(ULONGLONG) module_entry.modBaseAddr] = module_entry;
+    LoadedModule *lModule = new LoadedModule(
+        (ULONGLONG) module_entry.modBaseAddr, 
+        (ULONGLONG) module_entry.modBaseAddr + module_entry.modBaseSize,
+        module_entry.szExePath,
+        process_id
+        );
+    modulesMap[lModule->start] = lModule;
 
     while (Module32Next(hProcessSnapShot, &module_entry)) {
-        modulesMap[(ULONGLONG) module_entry.modBaseAddr] = module_entry;
-        modules++;
+        LoadedModule *lModule = new LoadedModule(
+            (ULONGLONG) module_entry.modBaseAddr, 
+            (ULONGLONG) module_entry.modBaseAddr + module_entry.modBaseSize,
+            module_entry.szExePath,
+            process_id
+            );
+        modulesMap[lModule->start] = lModule;
     }
 
     // Close the handle
     CloseHandle(hProcessSnapShot);
-    return modules;
+    return modulesMap.size();
 }
 
-HMODULE peconv::get_module_containing(ULONGLONG address, DWORD process_id)
+void peconv::ProcessModules::delete_mapping()
 {
-    std::map<ULONGLONG, MODULEENTRY32> modulesMap;
-    size_t modules_count = peconv::enum_modules_in_process(modulesMap, process_id);
-    if (modules_count == 0) {
-        return NULL;
-    }
-    std::map<ULONGLONG, MODULEENTRY32>::iterator itr = modulesMap.begin();
+    std::map<ULONGLONG, LoadedModule*>::iterator itr = modulesMap.begin();
     for (; itr != modulesMap.end(); itr++ ) {
-        ULONGLONG start = itr->first;
-        ULONGLONG end =  start + itr->second.modBaseSize;
+        const LoadedModule *module = itr->second;
+        delete module;
+    }
+    this->modulesMap.clear();
+}
 
-        if (address >= start && address < end) {
-            std::cout << "[*] Module:" << itr->second.szExePath << std::endl;
-            return (HMODULE) start;
+const peconv::LoadedModule* peconv::ProcessModules::get_module_containing(ULONGLONG address)
+{
+    std::map<ULONGLONG, LoadedModule*>::iterator start_itr = modulesMap.begin();
+    std::map<ULONGLONG, LoadedModule*>::iterator stop_itr = modulesMap.upper_bound(address);
+    std::map<ULONGLONG, LoadedModule*>::iterator itr = start_itr;
+    for (; itr != stop_itr; itr++ ) {
+        const LoadedModule *module = itr->second;
+
+        if (address >= module->start && address < module->end) {
+            return module;
         }
     }
-    return NULL;
+    return nullptr;
+}
+
+HMODULE peconv::get_module_containing(ULONGLONG address)
+{
+    ProcessModules modules(GetCurrentProcessId());
+    modules.load_mapping();
+    const LoadedModule* module = modules.get_module_containing(address);
+    if (module == nullptr) {
+        return nullptr;
+    }
+    return HMODULE(module->start);
 }
