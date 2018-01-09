1dc5dd0ccfd3381d843c335897d20093ab254111
hasherezade <hasherezade@gmail.com>
2018-01-09 23:45:32 +0100

    [REFACT] Introduced 2 types of buffer: unaligned and aligned

--- libpeconv/include/peconv/module_helper.h
+++ libpeconv/include/peconv/module_helper.h
@@ -4,15 +4,31 @@
 
 namespace peconv {
 
+// Aligned buffers - starting from the beginning of the new section£
+typedef PBYTE UNALIGNED_BUF;
+
+UNALIGNED_BUF alloc_unaligned(size_t buf_size);
+
+void free_unaligned(UNALIGNED_BUF section_buffer);
+
+// Unaligned buffers - not aligned to the beginning of section:
+typedef PBYTE ALIGNED_BUF;
+
+ALIGNED_BUF alloc_aligned(size_t buffer_size, DWORD protect, ULONGLONG desired_base=NULL);
+
+bool free_aligned(ALIGNED_BUF buffer, size_t buffer_size);
+
+
+//PE buffers (wrappers)
 //TODO: implement a class for module management
 
-BYTE* alloc_pe_buffer(size_t buffer_size, DWORD protect, ULONGLONG desired_base=NULL);
+ALIGNED_BUF alloc_pe_buffer(size_t buffer_size, DWORD protect, ULONGLONG desired_base=NULL);
 
 // Free loaded module (wrapper)
-bool free_pe_buffer(BYTE* buffer, size_t buffer_size);
+bool free_pe_buffer(ALIGNED_BUF buffer, size_t buffer_size);
 
-BYTE* alloc_pe_section(size_t buf_size);
+UNALIGNED_BUF alloc_pe_section(size_t buf_size);
 
-void free_pe_section(BYTE *section_buffer);
+void free_pe_section(UNALIGNED_BUF section_buffer);
 
 }; //namespace peconv
--- libpeconv/src/module_helper.cpp
+++ libpeconv/src/module_helper.cpp
@@ -2,28 +2,55 @@
 
 #include <iostream>
 
-BYTE* peconv::alloc_pe_buffer(size_t buffer_size, DWORD protect, ULONGLONG desired_base)
+peconv::UNALIGNED_BUF peconv::alloc_unaligned(size_t buf_size)
 {
-    PBYTE buffer = (BYTE*) VirtualAlloc((LPVOID) desired_base, buffer_size, MEM_COMMIT | MEM_RESERVE, protect);
-    return buffer;
+    PBYTE buf = (PBYTE) calloc(buf_size, sizeof(BYTE));
+    return buf;
 }
 
-bool peconv::free_pe_buffer(BYTE* buffer, size_t buffer_size)
+void peconv::free_unaligned(peconv::UNALIGNED_BUF section_buffer)
+{
+    free(section_buffer);
+}
+
+peconv::ALIGNED_BUF peconv::alloc_aligned(size_t buffer_size, DWORD protect, ULONGLONG desired_base)
+{
+    PBYTE buf = (PBYTE) VirtualAlloc((LPVOID) desired_base, buffer_size, MEM_COMMIT | MEM_RESERVE, protect);
+    return buf;
+}
+
+// Free loaded module (wrapper)
+bool peconv::free_aligned(peconv::ALIGNED_BUF buffer, size_t buffer_size)
 {
     if (buffer == NULL) return true;
     if (!VirtualFree(buffer, buffer_size, MEM_DECOMMIT)) {
+        std::cerr << "Releasing failed" << std::endl;
         return false;
     }
     return true;
 }
 
-BYTE* peconv::alloc_pe_section(size_t buf_size)
+//wrappers
+
+peconv::ALIGNED_BUF peconv::alloc_pe_buffer(size_t buffer_size, DWORD protect, ULONGLONG desired_base)
 {
-    return (BYTE*) calloc(buf_size, sizeof(BYTE));
+    return alloc_aligned(buffer_size, protect, desired_base);
 }
 
-void peconv::free_pe_section(BYTE *section_buffer)
+//-----------------------------------------------------------------------------------
+
+// Free loaded module (wrapper)
+bool peconv::free_pe_buffer(peconv::ALIGNED_BUF buffer, size_t buffer_size)
 {
-    free(section_buffer);
+    return peconv::free_aligned(buffer, buffer_size);
 }
 
+peconv::UNALIGNED_BUF peconv::alloc_pe_section(size_t buf_size)
+{
+    return peconv::alloc_unaligned(buf_size);
+}
+
+void peconv::free_pe_section(peconv::UNALIGNED_BUF section_buffer)
+{
+    return peconv::free_unaligned(section_buffer);
+}
