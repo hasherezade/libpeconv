3678cb6998c83184b7b1044a5708356fc5a8712b
hasherezade <hasherezade@gmail.com>
2018-03-29 17:44:16 +0200

    [FEATURE] In resource_util: added hModule parameter to load_resource_data

--- libpeconv/include/peconv/resource_util.h
+++ libpeconv/include/peconv/resource_util.h
@@ -10,9 +10,13 @@ namespace peconv {
     If out_size is not zero, it reads maximum out_size of bytes. If out_size is zero, it reads the full resource.
     The actual read size is returned back in out_size.
     Automatically allocates a buffer of the required size.
+    If hInstance is NULL, it search the resource in the current module. Otherwise, it search in the given module.
     */
-    peconv::ALIGNED_BUF load_resource_data(OUT size_t &out_size, const int res_id, const LPSTR res_type = RT_RCDATA);
+    peconv::ALIGNED_BUF load_resource_data(OUT size_t &out_size, const int res_id, const LPSTR res_type = RT_RCDATA, HMODULE hInstance = nullptr);
 
     void free_resource_data(peconv::ALIGNED_BUF buffer);
+
+    // a helper function to get the module handle of the current DLL:
+    HMODULE get_current_module_handle();
 
 }; //namespace peconv
--- libpeconv/src/resource_util.cpp
+++ libpeconv/src/resource_util.cpp
@@ -1,14 +1,38 @@
 #include "peconv/resource_util.h"
 
-peconv::ALIGNED_BUF peconv::load_resource_data(OUT size_t &out_size, int res_id, const LPSTR res_type)
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+HMODULE peconv::get_current_module_handle()
 {
-    HMODULE hInstance = GetModuleHandleA(NULL);
+    HMODULE hMod = NULL;
+    GetModuleHandleExW(
+        GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+        reinterpret_cast<LPCWSTR>(&peconv::get_current_module_handle),
+        &hMod);
+    return hMod;
+}
+
+peconv::ALIGNED_BUF peconv::load_resource_data(OUT size_t &out_size, int res_id, const LPSTR res_type, HMODULE hInstance)
+{
+    if (hInstance == nullptr) {
+        hInstance = GetModuleHandleA(NULL);
+    }
     HRSRC res = FindResourceA(hInstance, MAKEINTRESOURCE(res_id), res_type);
-    if (!res) return nullptr;
-
+    if (!res) {
+#ifdef _DEBUG
+        std::cerr << "Cannot find resource" << std::endl;
+#endif
+        return nullptr;
+    }
     HGLOBAL res_handle  = LoadResource(hInstance, res);
-    if (res_handle == nullptr) return nullptr;
-
+    if (res_handle == nullptr) {
+#ifdef _DEBUG
+        std::cerr << "Cannot get resource handle" << std::endl;
+#endif
+        return nullptr;
+    }
     BYTE* res_data = (BYTE*) LockResource(res_handle);
     size_t r_size = static_cast<size_t>(SizeofResource(hInstance, res));
     if (out_size != 0 && out_size <= r_size) {
