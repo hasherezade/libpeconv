5853bdc4b0587007697d70f1de4485758821b716
hasherezade <hasherezade@gmail.com>
2019-06-26 22:27:54 +0200

    [NOBIN] Normalized indentations in load_config_util/defs

--- libpeconv/include/peconv/load_config_util.h
+++ libpeconv/include/peconv/load_config_util.h
@@ -7,16 +7,16 @@
 
 namespace peconv {
 
-	typedef enum {
-		LOAD_CONFIG_NONE = 0,
-		LOAD_CONFIG_W7_VER = 7,
-		LOAD_CONFIG_W8_VER = 8,
-		LOAD_CONFIG_W10_VER = 10,
-		LOAD_CONFIG_UNK_VER = -1
-	} t_load_config_ver;
+    typedef enum {
+        LOAD_CONFIG_NONE = 0,
+        LOAD_CONFIG_W7_VER = 7,
+        LOAD_CONFIG_W8_VER = 8,
+        LOAD_CONFIG_W10_VER = 10,
+        LOAD_CONFIG_UNK_VER = -1
+    } t_load_config_ver;
 
-	BYTE* get_load_config_ptr(BYTE* buffer, size_t buf_size);
+    BYTE* get_load_config_ptr(BYTE* buffer, size_t buf_size);
 
-	t_load_config_ver get_load_config_version(BYTE* buffer, size_t buf_size, BYTE* ld_config_ptr);
+    t_load_config_ver get_load_config_version(BYTE* buffer, size_t buf_size, BYTE* ld_config_ptr);
 
 }; // namespace peconv
