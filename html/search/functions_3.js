8a73c22bf1704f1609402207a84b9b9bb16d6276
hasherezade <hasherezade@gmail.com>
2019-06-28 01:18:56 +0200

    Docs updated

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['detect_5fdump_5fmode',['detect_dump_mode',['../namespacepeconv.html#abe2bbe2e37060c67deeac1544110b573',1,'peconv']]],
-  ['dump_5fpe',['dump_pe',['../namespacepeconv.html#ac385e88a0c0fd8bac6ff6acab547c80c',1,'peconv']]],
+  ['detect_5fdump_5fmode',['detect_dump_mode',['../namespacepeconv.html#ab921893df2ddaeb33f3b376a04e5afde',1,'peconv']]],
+  ['dump_5fpe',['dump_pe',['../namespacepeconv.html#a3fcbd8235d3f9bbbde2caa1a091387bc',1,'peconv']]],
   ['dump_5fremote_5fpe',['dump_remote_pe',['../namespacepeconv.html#aa10cbf259ef601bdb3fe83233b955bc0',1,'peconv']]],
   ['dump_5fto_5ffile',['dump_to_file',['../namespacepeconv.html#a31bd1fafded9bb7f7d89b30679747a7f',1,'peconv']]]
 ];
