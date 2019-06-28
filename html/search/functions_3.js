e1d5acc05af35631237ba6637fe5e85df2afcdfd
hasherezade <hasherezade@gmail.com>
2019-06-28 15:39:08 +0200

    Updated docs. Changed directory structure.

--- html/search/functions_3.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['detect_5fdump_5fmode',['detect_dump_mode',['../namespacepeconv.html#ab921893df2ddaeb33f3b376a04e5afde',1,'peconv']]],
-  ['dump_5fpe',['dump_pe',['../namespacepeconv.html#a3fcbd8235d3f9bbbde2caa1a091387bc',1,'peconv']]],
-  ['dump_5fremote_5fpe',['dump_remote_pe',['../namespacepeconv.html#aa10cbf259ef601bdb3fe83233b955bc0',1,'peconv']]],
-  ['dump_5fto_5ffile',['dump_to_file',['../namespacepeconv.html#a31bd1fafded9bb7f7d89b30679747a7f',1,'peconv']]]
-];
--- html/search/functions_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['map_5faddresses_5fto_5ffunctions',['map_addresses_to_functions',['../fix__imports_8cpp.html#ac9d621126f72b3d4363a96ab6547aa53',1,'fix_imports.cpp']]],
-  ['mapaddressestofunctions',['mapAddressesToFunctions',['../classpeconv_1_1_imported_dll_coverage.html#a876acd96b9dc921a79043727ec0ad2ad',1,'peconv::ImportedDllCoverage']]]
-];
