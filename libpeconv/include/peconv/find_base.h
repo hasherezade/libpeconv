3746f449e2d41942337fdc036094ef5bd828c7b3
hasherezade <hasherezade@gmail.com>
2020-02-26 00:28:38 +0100

    [NOBIN] Fixed indentation

--- libpeconv/include/peconv/find_base.h
+++ libpeconv/include/peconv/find_base.h
@@ -4,6 +4,6 @@
 
 namespace peconv {
     // Try to find a base to which the PE file was relocated, basing on the relocations.
-	// WARNING: sometimes it may give inaccurate results
+    // WARNING: sometimes it may give inaccurate results
     ULONGLONG find_base_candidate(BYTE *buf, size_t buf_size);
 };
