e15a7f49b492df4452068681e9e5aad71e93e58b
hasherezade <hasherezade@gmail.com>
2020-07-02 05:19:36 +0200

    [FEATURE] Added find_base to the main header

--- libpeconv/include/peconv/find_base.h
+++ libpeconv/include/peconv/find_base.h
@@ -11,7 +11,7 @@ namespace peconv {
 
     /**
     Try to find a base to which the PE file was relocated, basing on the filled relocations.
-    WARNING: sometimes it may give inaccurate results.
+    WARNING: the found base is an estimate, and sometimes may not be fully accurate.
     \param module_ptr : the module which's base is being searched
     \param module_size : the size of the given module
     \return the base to which the module was relocated
