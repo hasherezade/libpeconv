1e4d0a15f65ba20a0d0052592b25f7fa2e56a4ea
hasherezade <hasherezade@users.noreply.github.com>
2021-12-27 15:01:02 +0000

    deploy: bb55c735778fcb7203b10d071ca95f0e38f1e499

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['collectcoderelocs_0',['CollectCodeRelocs',['../classpeconv_1_1_collect_code_relocs.html',1,'peconv']]]
+  ['collectcoderelocs_0',['CollectCodeRelocs',['../classpeconv_1_1_collect_code_relocs.html',1,'peconv']]],
+  ['collectthunkscallback_1',['CollectThunksCallback',['../class_collect_thunks_callback.html',1,'']]]
 ];
