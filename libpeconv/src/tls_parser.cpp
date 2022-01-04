e09b82bbf34885322439bfa6820083bbd0663821
hasherezade <hasherezade@gmail.com>
2022-01-04 17:07:58 +0100

    [REFACT] In list_tls_callbacks: list addresses in their original form

--- libpeconv/src/tls_parser.cpp
+++ libpeconv/src/tls_parser.cpp
@@ -5,26 +5,9 @@
 
 namespace peconv {
 
-    // convert a virtual address (given as VA or RVA) to RVA
-    bool virtual_addr_to_rva(IN const ULONGLONG img_base, IN const DWORD img_size, IN ULONGLONG callback_addr, OUT DWORD &callback_rva)
-    {
-        if (!img_size || !callback_addr) return false;
-
-        //check if VA:
-        if (callback_addr >= img_base && callback_addr < (img_base + img_size)) {
-            callback_rva = MASK_TO_DWORD(callback_addr - img_base);
-            return true;
-        }
-        if (callback_addr < img_size) {
-            callback_rva = MASK_TO_DWORD(callback_addr);
-            return true;
-        }
-        // out of scope address
-        return false;
-    }
 
     template <typename FIELD_T>
-    size_t fetch_callbacks_list(IN PVOID modulePtr, IN size_t moduleSize, IN DWORD callbacks_rva, OUT std::vector<DWORD> &callbacks_RVAs)
+    size_t fetch_callbacks_list(IN PVOID modulePtr, IN size_t moduleSize, IN DWORD callbacks_rva, OUT std::vector<ULONGLONG> &tls_callbacks)
     {
         FIELD_T* callbacks_list_ptr = (FIELD_T*)(callbacks_rva + (BYTE*)modulePtr);
         if (!validate_ptr(modulePtr, moduleSize, callbacks_list_ptr, sizeof(FIELD_T))) {
@@ -41,20 +24,32 @@ namespace peconv {
             FIELD_T value = *next_callback;
             if (value == 0) break;
 
-            DWORD callback_rva = 0;
-            if (!virtual_addr_to_rva(img_base, img_size, value, callback_rva)) {
-                // in some cases, TLS callbacks can lead to functions in other modules: we want to skip those,
-                // keeping only addresses that are in the current PE scope
-                continue;
-            }
-            callbacks_RVAs.push_back(callback_rva);
+            tls_callbacks.push_back(value);
             counter++;
         }
         return counter;
     }
 };
 
-size_t peconv::list_tls_callbacks(IN PVOID modulePtr, IN size_t moduleSize, OUT std::vector<DWORD> &callbacks_RVAs)
+
+bool peconv::virtual_addr_to_rva(IN const ULONGLONG img_base, IN const DWORD img_size, IN ULONGLONG callback_addr, OUT DWORD &callback_rva)
+{
+    if (!img_size || !callback_addr) return false;
+
+    //check if VA:
+    if (callback_addr >= img_base && callback_addr < (img_base + img_size)) {
+        callback_rva = MASK_TO_DWORD(callback_addr - img_base);
+        return true;
+    }
+    if (callback_addr < img_size) {
+        callback_rva = MASK_TO_DWORD(callback_addr);
+        return true;
+    }
+    // out of scope address
+    return false;
+}
+
+size_t peconv::list_tls_callbacks(IN PVOID modulePtr, IN size_t moduleSize, OUT std::vector<ULONGLONG> &tls_callbacks)
 {
     const ULONGLONG img_base = (ULONGLONG)modulePtr;
     const DWORD img_size = peconv::get_image_size((BYTE*)modulePtr);
@@ -78,32 +73,42 @@ size_t peconv::list_tls_callbacks(IN PVOID modulePtr, IN size_t moduleSize, OUT 
 #endif
     size_t counter = 0;
     if (peconv::is64bit((BYTE*)modulePtr)) {
-        counter = fetch_callbacks_list<ULONGLONG>(modulePtr, moduleSize, callbacks_rva, callbacks_RVAs);
+        counter = fetch_callbacks_list<ULONGLONG>(modulePtr, moduleSize, callbacks_rva, tls_callbacks);
     }
     else {
-        counter = fetch_callbacks_list<DWORD>(modulePtr, moduleSize, callbacks_rva, callbacks_RVAs);
+        counter = fetch_callbacks_list<DWORD>(modulePtr, moduleSize, callbacks_rva, tls_callbacks);
     }
     return counter;
 }
 
 size_t peconv::run_tls_callbacks(IN PVOID modulePtr, IN size_t moduleSize, IN DWORD dwReason)
 {
+    const size_t img_size = peconv::get_image_size((BYTE*)modulePtr);
     if (moduleSize == 0) {
-        moduleSize = peconv::get_image_size((BYTE*)modulePtr);
+        moduleSize = img_size;
     }
-    std::vector<DWORD> callbacksRVAs;
-    if (!peconv::list_tls_callbacks(modulePtr, moduleSize, callbacksRVAs)) {
+    std::vector<ULONGLONG> tls_callbacks;
+    if (!peconv::list_tls_callbacks(modulePtr, moduleSize, tls_callbacks)) {
         return 0;
     }
-    std::vector<DWORD>::iterator itr;
+    std::vector<ULONGLONG>::iterator itr;
     size_t i = 0;
-    for (itr = callbacksRVAs.begin(); itr != callbacksRVAs.end(); ++itr, i++) {
-        DWORD rva = *itr;
+    for (itr = tls_callbacks.begin(); itr != tls_callbacks.end(); ++itr, i++) {
+        ULONGLONG callback_addr = *itr;
+        DWORD rva = 0; //TLS callback can be defined as RVA or VA, so make sure it is in a consistent format...
+        if (!peconv::virtual_addr_to_rva((ULONG_PTR)modulePtr, img_size, callback_addr, rva)) {
+            // in some cases, TLS callbacks can lead to functions in other modules: we want to skip those,
+            // keeping only addresses that are in the current PE scope
+            continue;
+        }
 #ifdef _DEBUG
         std::cout << std::hex << "TLS RVA:" << rva << std::endl;
 #endif
         ULONG_PTR callback_va = rva + (ULONG_PTR)modulePtr;
-
+        if (!validate_ptr(modulePtr, moduleSize, (BYTE*)callback_va, sizeof(BYTE))) {
+            // make sure that the address is valid
+            continue;
+        }
         void(NTAPI *callback_func)(PVOID DllHandle, DWORD dwReason, PVOID) = (void(NTAPI *)(PVOID, DWORD, PVOID)) (callback_va);
 #ifdef _DEBUG
         std::cout << "Calling TLS callback[" << i << "]:" << std::endl;
