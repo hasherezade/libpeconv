7e01cb1483cf02e9f31c264eaed28ff6eb8b6259
hasherezade <hasherezade@gmail.com>
2022-01-09 17:56:10 -0800

    [NOBIN] Improved comments

--- libpeconv/include/peconv/tls_parser.h
+++ libpeconv/include/peconv/tls_parser.h
@@ -11,7 +11,7 @@
 namespace peconv {
 
     /**
-    A helper function, normalizing virtual addresses. It automatically detects if the given virtual_addr is VA or RVA, and converts it into RVA
+    A helper function, normalizing virtual addresses. It automatically detects if the given virtual address is VA or RVA, and converts it into RVA
     \param imgBase : the base address to which the module was relocated
     \param imgSize : the size of the image
     \param virtualAddr : the virtual address (RVA or VA) that we want to convert (within the module described by imgBase and imgSize)
@@ -25,6 +25,7 @@ namespace peconv {
     \param modulePtr : pointer to the buffer with the PE in a Virtual format, relocated to the load base
     \param moduleSize : size of the given module (if 0 given, the imageSize from the PE headers will be used)
     \param tls_callbacks : a vector of TLS callbacks addresses (as given in the TLS table)
+    \return number of TLS callbacks added to the list
     */
     size_t list_tls_callbacks(IN PVOID modulePtr, IN size_t moduleSize, OUT std::vector<ULONGLONG> &tls_callbacks);
 
@@ -33,6 +34,7 @@ namespace peconv {
     \param modulePtr : pointer to the buffer with the PE in a Virtual format, relocated to the load base
     \param moduleSize : size of the given module (if 0 given, the imageSize from the PE headers will be used)
     \param dwReason : a parameter (dwReason) that will be passed to the callback function
+    \return number of TLS callbacks executed
     */
     size_t run_tls_callbacks(IN PVOID modulePtr, IN size_t moduleSize=0, IN DWORD dwReason = DLL_PROCESS_ATTACH);
 
