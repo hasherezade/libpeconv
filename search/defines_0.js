7d530f901aa9b14812939bceae142a8cdfd2acbf
hasherezade <hasherezade@users.noreply.github.com>
2022-01-10 00:20:19 +0000

    deploy: b4a73532a0317584c5e62cfa40533111466191b6

--- search/defines_0.js
+++ search/defines_0.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['mask_5fto_5fdword_0',['MASK_TO_DWORD',['../pe__hdrs__helper_8h.html#aaa26768a6583c35dd045af63f9207edd',1,'pe_hdrs_helper.h']]],
   ['mask_5fto_5fword_1',['MASK_TO_WORD',['../pe__hdrs__helper_8h.html#a0b697bc33c89771cfb9813059957b230',1,'pe_hdrs_helper.h']]],
-  ['min_5fdll_5flen_2',['MIN_DLL_LEN',['../fix__imports_8h.html#af0e2f31bb97b6213d7dfa6511915f4d7',1,'fix_imports.h']]]
+  ['max_5fdword_2',['MAX_DWORD',['../pe__hdrs__helper_8h.html#a6966065b527fa9cd37c73ac00255590f',1,'pe_hdrs_helper.h']]],
+  ['max_5fword_3',['MAX_WORD',['../pe__hdrs__helper_8h.html#a95b015e2f596dc86ea9220be04427e1a',1,'pe_hdrs_helper.h']]],
+  ['min_5fdll_5flen_4',['MIN_DLL_LEN',['../fix__imports_8h.html#af0e2f31bb97b6213d7dfa6511915f4d7',1,'fix_imports.h']]]
 ];
