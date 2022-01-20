073ac7d26e68580af6b6470295c57f6b311f17c9
hasherezade <hasherezade@users.noreply.github.com>
2022-01-20 16:25:31 +0000

    deploy: 03595977c26147bdd7e1e284a2c1374e16368805

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['add_5fhook_0',['add_hook',['../classpeconv_1_1hooking__func__resolver.html#a89d7be039ca86e2a3df5c37884ebd7ee',1,'peconv::hooking_func_resolver']]],
-  ['add_5fto_5flookup_1',['add_to_lookup',['../classpeconv_1_1_exports_mapper.html#a66bec297143444676b938fea64a5dc31',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr, ULONGLONG moduleBase)'],['../classpeconv_1_1_exports_mapper.html#a698397923acaf76c8dfe0d2767842248',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr)']]],
+  ['add_5fto_5flookup_1',['add_to_lookup',['../classpeconv_1_1_exports_mapper.html#a591acd077a24b6da3d74d524963d1201',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr, size_t moduleSize, ULONGLONG moduleBase)'],['../classpeconv_1_1_exports_mapper.html#a468c16f23b12f93c9974e8f54d1ecd83',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr, ULONGLONG moduleBase)'],['../classpeconv_1_1_exports_mapper.html#a698397923acaf76c8dfe0d2767842248',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr)']]],
   ['addresses_2',['addresses',['../classpeconv_1_1_imported_dll_coverage.html#ab3f8575995b216c18a86085791fdf16d',1,'peconv::ImportedDllCoverage']]],
   ['addrtofunc_3',['addrToFunc',['../classpeconv_1_1_imported_dll_coverage.html#aba9ec06f5ab9e18f8078bdeada9ed4fb',1,'peconv::ImportedDllCoverage']]],
   ['aligned_5fbuf_4',['ALIGNED_BUF',['../namespacepeconv.html#a4389e097c30b01568bf10f4d07a50b42',1,'peconv']]],
@@ -11,7 +11,7 @@ var searchData=
   ['alloc_5funaligned_8',['alloc_unaligned',['../namespacepeconv.html#a4d50b95546cf5fa78ce185ce1acbb8ec',1,'peconv']]],
   ['apply_5frelocations_9',['apply_relocations',['../relocate_8cpp.html#a66ddf83b81ed1c6787ffb1421a4f94b5',1,'relocate.cpp']]],
   ['applybackup_10',['applyBackup',['../classpeconv_1_1_patch_backup.html#a39e33bcc6a1558a6768357ddf4ffbb50',1,'peconv::PatchBackup']]],
-  ['applyreloccallback_11',['ApplyRelocCallback',['../class_apply_reloc_callback.html',1,'ApplyRelocCallback'],['../class_apply_reloc_callback.html#a09cde4a31ccf84a41b1343cd8ce0d646',1,'ApplyRelocCallback::ApplyRelocCallback()']]],
+  ['applyreloccallback_11',['ApplyRelocCallback',['../class_apply_reloc_callback.html#a09cde4a31ccf84a41b1343cd8ce0d646',1,'ApplyRelocCallback::ApplyRelocCallback()'],['../class_apply_reloc_callback.html',1,'ApplyRelocCallback']]],
   ['associatevaandfunc_12',['associateVaAndFunc',['../classpeconv_1_1_exports_mapper.html#a6af4b7af3e750b72615c33969a27ef72',1,'peconv::ExportsMapper']]],
   ['attributes_13',['Attributes',['../struct___i_m_a_g_e___d_e_l_a_y_l_o_a_d___d_e_s_c_r_i_p_t_o_r.html#a33f1b4a9ceedd55888721a086fd60dd1',1,'_IMAGE_DELAYLOAD_DESCRIPTOR']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['add_5fhook_0',['add_hook',['../classpeconv_1_1hooking__func__resolver.html#a89d7be039ca86e2a3df5c37884ebd7ee',1,'peconv::hooking_func_resolver']]],
-  ['add_5fto_5flookup_1',['add_to_lookup',['../classpeconv_1_1_exports_mapper.html#a66bec297143444676b938fea64a5dc31',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr, ULONGLONG moduleBase)'],['../classpeconv_1_1_exports_mapper.html#a698397923acaf76c8dfe0d2767842248',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr)']]],
+  ['add_5fto_5flookup_1',['add_to_lookup',['../classpeconv_1_1_exports_mapper.html#a591acd077a24b6da3d74d524963d1201',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr, size_t moduleSize, ULONGLONG moduleBase)'],['../classpeconv_1_1_exports_mapper.html#a468c16f23b12f93c9974e8f54d1ecd83',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr, ULONGLONG moduleBase)'],['../classpeconv_1_1_exports_mapper.html#a698397923acaf76c8dfe0d2767842248',1,'peconv::ExportsMapper::add_to_lookup(std::string moduleName, HMODULE modulePtr)']]],
   ['alloc_5faligned_2',['alloc_aligned',['../namespacepeconv.html#a3df24248b3c8c9be64db51a405486923',1,'peconv']]],
   ['alloc_5fpe_5fbuffer_3',['alloc_pe_buffer',['../namespacepeconv.html#a92647f207ca261ca38ff516943501dd1',1,'peconv']]],
   ['alloc_5funaligned_4',['alloc_unaligned',['../namespacepeconv.html#a4d50b95546cf5fa78ce185ce1acbb8ec',1,'peconv']]],
