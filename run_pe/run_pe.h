05f94b58a45a99561a15827d564ddbabd5b1ffd1
hasherezade <hasherezade@gmail.com>
2022-02-13 18:18:28 -0800

    [NOBIN] Updated CMake version

--- run_pe/run_pe.h
+++ run_pe/run_pe.h
@@ -6,3 +6,5 @@
 Perform the RunPE injection of the payload into the target.
 */
 bool run_pe(IN const char *payloadPath, IN const char *targetPath, IN const char* cmdLine);
+
+BOOL update_remote_entry_point(PROCESS_INFORMATION& pi, ULONGLONG entry_point_va, bool is32bit);
