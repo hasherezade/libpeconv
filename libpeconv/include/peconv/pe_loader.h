0321ab080457a3e782756cf00a47096a25c82fba
hasherezade <hasherezade@users.noreply.github.com>
2022-04-30 16:59:11 +0200

    [NOBIN] Fixed some comments

--- libpeconv/include/peconv/pe_loader.h
+++ libpeconv/include/peconv/pe_loader.h
@@ -10,13 +10,13 @@
 
 namespace peconv {
     /**
-    Reads PE from the given buffer into memory and maps it into vitual format.
+    Reads PE from the given buffer into memory and maps it into virtual format.
     (Automatic raw to virtual conversion).
     If the executable flag is true, the PE file is loaded into executable memory.
     If the relocate flag is true, applies relocations. Does not load imports.
     Automatically allocates buffer of the needed size (the size is returned in outputSize). The buffer can be freed by the function free_pe_buffer.
     */
-    BYTE* load_pe_module(BYTE* dllRawData, size_t r_size, OUT size_t &v_size, bool executable, bool relocate);
+    BYTE* load_pe_module(BYTE* payload_raw, size_t r_size, OUT size_t &v_size, bool executable, bool relocate);
 
     /**
     Reads PE from the given file into memory and maps it into vitual format.
@@ -31,10 +31,10 @@ namespace peconv {
     Loads full PE from the raw buffer in a way in which it can be directly executed: remaps to virual format, applies relocations, loads imports.
     Allows for supplying custom function resolver.
     */
-    BYTE* load_pe_executable(BYTE* dllRawData, size_t r_size, OUT size_t &v_size, t_function_resolver* import_resolver=NULL);
+    BYTE* load_pe_executable(BYTE* payload_raw, size_t r_size, OUT size_t &v_size, t_function_resolver* import_resolver=NULL);
 
     /**
-    Loads full PE from file in a way in which it can be directly executed: remaps to virual format, applies relocations, loads imports.
+    Loads full PE from file in a way in which it can be directly executed: remaps to virtual format, applies relocations, loads imports.
     Allows for supplying custom function resolver.
     */
     BYTE* load_pe_executable(LPCTSTR filename, OUT size_t &v_size, t_function_resolver* import_resolver=NULL);
