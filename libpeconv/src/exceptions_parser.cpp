3e4d7aa8b49c68cdda5ba8d3936b503ba071f9da
NotCapengeR <kneg3kzj7lmp@mail.ru>
2024-12-04 14:58:02 +0500

    Space fix

--- libpeconv/src/exceptions_parser.cpp
+++ libpeconv/src/exceptions_parser.cpp
@@ -8,6 +8,10 @@
 #include <iostream>
 #endif
 
+#ifndef min
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
 namespace details {
 #define RTL_VERIFY_FLAGS_MAJOR_VERSION	0
 #define RTL_VERIFY_FLAGS_MINOR_VERSION	1
