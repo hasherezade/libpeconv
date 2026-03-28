049f69b82c27f5b6e2e354e2b5ed84d6a7c83a09
hasherezade <hasherezade@users.noreply.github.com>
2026-03-28 21:58:16 +0000

    deploy: 337f81501524f4033175bf237b02226d58579ae0

--- search/all_15.js
+++ search/all_15.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['_7eimportscollection_0',['~ImportsCollection',['../classpeconv_1_1_imports_collection.html#a5bfa043f8b28983e99e287985b71d7cf',1,'peconv::ImportsCollection']]],
+  ['_7eimportscollection_0',['~ImportsCollection',['../classpeconv_1_1_imports_collection.html#a88c605f4ad77501d420ca7386c866c2b',1,'peconv::ImportsCollection']]],
   ['_7eimportsuneraser_1',['~ImportsUneraser',['../classpeconv_1_1_imports_uneraser.html#a5873eb9b5f819f1bb5aa095617a9bae4',1,'peconv::ImportsUneraser']]],
   ['_7eimportthunkscallback_2',['~ImportThunksCallback',['../classpeconv_1_1_import_thunks_callback.html#a5e3687c97d2e142947b7d4444fd97235',1,'peconv::ImportThunksCallback']]],
   ['_7eimpsnotcovered_3',['~ImpsNotCovered',['../classpeconv_1_1_imps_not_covered.html#a33a6e5e6ca0c4adcee0df0ab19a58e9d',1,'peconv::ImpsNotCovered']]],
-  ['_7epatchbackup_4',['~PatchBackup',['../classpeconv_1_1_patch_backup.html#ac9a98581426ad77b6544bad206a71e74',1,'peconv::PatchBackup']]],
+  ['_7epatchbackup_4',['~PatchBackup',['../classpeconv_1_1_patch_backup.html#a769f12e2b064c6753bbba9b1e8c75deb',1,'peconv::PatchBackup']]],
   ['_7erelocblockcallback_5',['~RelocBlockCallback',['../classpeconv_1_1_reloc_block_callback.html#a2f106a2cab2abf854dde6bf8b1ad786e',1,'peconv::RelocBlockCallback']]],
   ['_7esectionlocker_6',['~SectionLocker',['../class_section_locker.html#ab6e725b899702cf9c3fe0f068ab64520',1,'SectionLocker']]],
   ['_7et_5ffunction_5fresolver_7',['~t_function_resolver',['../classpeconv_1_1t__function__resolver.html#a2d33632db95581e5f25460e337539af4',1,'peconv::t_function_resolver']]]
