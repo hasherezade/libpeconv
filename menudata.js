72ffb87f0459761e58930e5e2c43715d3d39996c
hasherezade <hasherezade@users.noreply.github.com>
2026-03-28 15:12:20 +0000

    deploy: d73a96d0bc7842cd7596d7b8e9d7de78f1da9ad6

--- menudata.js
+++ menudata.js
@@ -167,6 +167,7 @@ var menudata={children:[
 {text:"Typedefs",url:"globals_type.html"},
 {text:"Macros",url:"globals_defs.html",children:[
 {text:"_",url:"globals_defs.html#index__5F"},
+{text:"i",url:"globals_defs.html#index_i"},
 {text:"l",url:"globals_defs.html#index_l"},
 {text:"m",url:"globals_defs.html#index_m"},
 {text:"p",url:"globals_defs.html#index_p"},
--- search/defines_3.js
+++ search/defines_3.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['page_5fsize_0',['PAGE_SIZE',['../pe__hdrs__helper_8h.html#a7d467c1d283fdfa1f2081ba1e0d01b6e',1,'pe_hdrs_helper.h']]],
-  ['peconv_5fforceinline_1',['PECONV_FORCEINLINE',['../exceptions__parser_8h.html#a1a05bebca4d63ea777ac045034d4cd38',1,'exceptions_parser.h']]],
-  ['peconv_5ftry_5fexcept_5fblock_5fend_2',['PECONV_TRY_EXCEPT_BLOCK_END',['../exceptions__parser_8h.html#ade026644fa93062708668acfae05c105',1,'exceptions_parser.h']]],
-  ['peconv_5ftry_5fexcept_5fblock_5fstart_3',['PECONV_TRY_EXCEPT_BLOCK_START',['../exceptions__parser_8h.html#af2e2212e22f7de5cda2d93545b97ce3a',1,'exceptions_parser.h']]]
+  ['mask_5fto_5fdword_0',['MASK_TO_DWORD',['../buffer__util_8h.html#aaa26768a6583c35dd045af63f9207edd',1,'buffer_util.h']]],
+  ['mask_5fto_5fword_1',['MASK_TO_WORD',['../buffer__util_8h.html#a0b697bc33c89771cfb9813059957b230',1,'buffer_util.h']]],
+  ['max_5fdword_2',['MAX_DWORD',['../buffer__util_8h.html#a6966065b527fa9cd37c73ac00255590f',1,'buffer_util.h']]],
+  ['max_5fword_3',['MAX_WORD',['../buffer__util_8h.html#a95b015e2f596dc86ea9220be04427e1a',1,'buffer_util.h']]],
+  ['min_4',['min',['../exceptions__parser_8cpp.html#ac6afabdc09a49a433ee19d8a9486056d',1,'exceptions_parser.cpp']]],
+  ['min_5fdll_5flen_5',['MIN_DLL_LEN',['../fix__imports_8h.html#af0e2f31bb97b6213d7dfa6511915f4d7',1,'fix_imports.h']]]
 ];
