28173344f1a6cb854d57b7db4f8322faeeab3278
hasherezade <hasherezade@users.noreply.github.com>
2026-03-28 15:19:50 +0000

    deploy: f0c4ff30ac04a0925d60fd1ed726e5008834e703

--- search/functions_14.js
+++ search/functions_14.js
@@ -3,5 +3,6 @@ var searchData=
   ['_7eimportscollection_0',['~ImportsCollection',['../structpeconv_1_1_imports_collection.html#a88c605f4ad77501d420ca7386c866c2b',1,'peconv::ImportsCollection']]],
   ['_7eimpsnotcovered_1',['~ImpsNotCovered',['../classpeconv_1_1_imps_not_covered.html#a33a6e5e6ca0c4adcee0df0ab19a58e9d',1,'peconv::ImpsNotCovered']]],
   ['_7epatchbackup_2',['~PatchBackup',['../classpeconv_1_1_patch_backup.html#a769f12e2b064c6753bbba9b1e8c75deb',1,'peconv::PatchBackup']]],
-  ['_7esectionlocker_3',['~SectionLocker',['../class_section_locker.html#ab6e725b899702cf9c3fe0f068ab64520',1,'SectionLocker']]]
+  ['_7esectionlocker_3',['~SectionLocker',['../class_section_locker.html#ab6e725b899702cf9c3fe0f068ab64520',1,'SectionLocker']]],
+  ['_7et_5ffunction_5fresolver_4',['~t_function_resolver',['../classpeconv_1_1t__function__resolver.html#a2d33632db95581e5f25460e337539af4',1,'peconv::t_function_resolver']]]
 ];
