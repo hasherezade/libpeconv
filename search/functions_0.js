dc75c147dc852374c1e44e04420d517ce4b5309d
hasherezade <hasherezade@users.noreply.github.com>
2026-04-17 23:13:00 +0000

    deploy: 5251cde55b8a4afcd4b6784eb37f21d366802e7b

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['_5ffetch_5fregion_5fsize_0',['_fetch_region_size',['../remote__pe__reader_8cpp.html#ad61b7aa751015bcc6c8bf66b6df2faee',1,'remote_pe_reader.cpp']]],
-  ['_5fsearch_5freadable_5fsize_1',['_search_readable_size',['../namespacepeconv.html#ab650ce67e446fe3ece6ced8fe56a912f',1,'peconv']]]
+  ['_5flist_5ftls_5fcallbacks_1',['_list_tls_callbacks',['../namespacepeconv.html#ac2c5524542e03df1babb356414145e2e',1,'peconv']]],
+  ['_5fsearch_5freadable_5fsize_2',['_search_readable_size',['../namespacepeconv.html#ab650ce67e446fe3ece6ced8fe56a912f',1,'peconv']]]
 ];
